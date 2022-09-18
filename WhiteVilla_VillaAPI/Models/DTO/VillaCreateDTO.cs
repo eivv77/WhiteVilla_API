c5ae2dabe42ecd5d594cd5ac021f64a3a4a319b5
Нурлан Эйвазов <nurlan4ik-eyvazov@mail.ru>
2022-09-18 18:56:02 +0400

    merge in master all

--- WhiteVilla_VillaAPI/Models/DTO/VillaCreateDTO.cs
+++ WhiteVilla_VillaAPI/Models/DTO/VillaCreateDTO.cs
@@ -9,7 +9,7 @@ namespace WhiteVilla_VillaAPI.Models.DTO
         public string Name { get; set; }
         public string Details { get; set; }
         [Required]
-        public double Rate { get; set; } //оценка
+        public double Rate { get; set; } //оценка 
         public int Occupancy { get; set; }
         public int Sqft { get; set; }
         public string ImgUrl { get; set; }
