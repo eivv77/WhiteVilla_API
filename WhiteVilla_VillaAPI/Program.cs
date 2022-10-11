3f9be5cc2dd3004450fe07373010e9c2f5c7390f
eivv77 <dlya.kf.2002@mail.ru>
2022-10-11 13:51:48 +0400

    add the fields in swagger header

--- WhiteVilla_VillaAPI/Program.cs
+++ WhiteVilla_VillaAPI/Program.cs
@@ -92,6 +92,23 @@ builder.Services.AddSwaggerGen(options => {
             new List<string>()
         }
     });
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "White Villa", 
+        Description = "API to manage Villa", 
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Dotnetmastery",
+            Url = new Uri("https://dotnetmastery.com")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "License",
+            Url = new Uri("https://example.com/license")
+        },
+    });
 });
 
 var app = builder.Build();
@@ -100,7 +117,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "White_VillaV1");
+    });
 }
 
 app.UseHttpsRedirection();
